84e46ac0fbbe06941e9e1a39811c917ee88f60fb
mmldb <arpadi.tamas@gmail.com>
2026-08-02 23:05:44 +0200

    Update PWA asset places.js

---
+++ places.js
@@ -0,0 +1,381 @@
+window.PLACES = [
+  {
+    "id": "jegenye",
+    "name": "Jegenye-völgy / Paprikás-patak",
+    "area": "Solymár",
+    "driveMin": 25,
+    "drive": "20–25 perc",
+    "distance": "near",
+    "water": "alacsony-közepes",
+    "dam": 4,
+    "toddler": "hordozó vagy kézenfogás",
+    "tag": "közeli",
+    "initialDone": true,
+    "startName": "Rózsika utca parkoló",
+    "start": [
+      47.5765899,
+      18.9379942
+    ],
+    "targetName": "Jegenye-völgyi vízesés",
+    "target": [
+      47.5798486,
+      18.9398565
+    ],
+    "note": "Rövid, visszafordulós, vízeséses mini séta. Szárazságban lehet kevés víz, de logisztikailag az egyik legkönnyebb.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Jegenye-v%C3%B6lgyi_v%C3%ADzes%C3%A9s_20170723_153629.jpg/960px-Jegenye-v%C3%B6lgyi_v%C3%ADzes%C3%A9s_20170723_153629.jpg",
+    "credit": "Wikimedia Commons",
+    "mapUrl": "https://maps.google.com/?q=47.5798486,18.9398565",
+    "startUrl": "https://maps.google.com/?q=47.5765899,18.9379942",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=Jegenye-v%C3%B6lgy%20/%20Paprik%C3%A1s-patak%20gyerekkel%20patak",
+    "gpxUrl": "gpx/jegenye.gpx"
+  },
+  {
+    "id": "domorkapu",
+    "name": "Dömörkapu / Bükkös-patak",
+    "area": "Szentendre",
+    "driveMin": 40,
+    "drive": "~40 perc",
+    "distance": "near",
+    "water": "közepes",
+    "dam": 5,
+    "toddler": "hordozó/kézenfogás",
+    "tag": "patakos",
+    "startName": "Dömörkapu",
+    "start": [
+      47.6929615,
+      19.0053947
+    ],
+    "targetName": "Bükkös-patak / Dömörkapu",
+    "target": [
+      47.6929615,
+      19.0053947
+    ],
+    "note": "Nagyon jó köves-patakos, gátépítős hely. Nem kell nagy túra, elég a patakparton bóklászni.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Szentendre%2C_D%C3%B6m%C3%B6rkapu%2C_a_v%C3%ADzes%C3%A9s._Fortepan_4231.jpg/960px-Szentendre%2C_D%C3%B6m%C3%B6rkapu%2C_a_v%C3%ADzes%C3%A9s._Fortepan_4231.jpg",
+    "credit": "Wikimedia Commons / Fortepan",
+    "mapUrl": "https://maps.google.com/?q=47.6929615,19.0053947",
+    "startUrl": "https://maps.google.com/?q=47.6929615,19.0053947",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=D%C3%B6m%C3%B6rkapu%20/%20B%C3%BCkk%C3%B6s-patak%20gyerekkel%20patak",
+    "gpxUrl": "gpx/domorkapu.gpx"
+  },
+  {
+    "id": "dera",
+    "name": "Dera-szurdok",
+    "area": "Pilisszentkereszt",
+    "driveMin": 45,
+    "drive": "40–45 perc",
+    "distance": "near",
+    "water": "közepes",
+    "dam": 4,
+    "toddler": "inkább hordozó",
+    "tag": "kalandos",
+    "startName": "Pilisszentkereszt / Szurdok",
+    "start": [
+      47.6845258,
+      18.9172138
+    ],
+    "targetName": "Dera-szurdok",
+    "target": [
+      47.6845258,
+      18.9172138
+    ],
+    "note": "Szurdokos, hidacskás, patakátkelős. 3,5 évesnek szuper; 1,5 éves mellett figyelős.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Dera-szurdok.jpg/960px-Dera-szurdok.jpg",
+    "credit": "Wikimedia Commons",
+    "mapUrl": "https://maps.google.com/?q=47.6845258,18.9172138",
+    "startUrl": "https://maps.google.com/?q=47.6845258,18.9172138",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=Dera-szurdok%20gyerekkel%20patak",
+    "gpxUrl": "gpx/dera.gpx"
+  },
+  {
+    "id": "holdvilag",
+    "name": "Holdvilág-árok",
+    "area": "Pomáz / Kiskovácsi",
+    "driveMin": 40,
+    "drive": "~40 perc",
+    "distance": "near",
+    "water": "közepes",
+    "dam": 3,
+    "toddler": "csak hordozó",
+    "tag": "csak Döbivel",
+    "startName": "Holdvilág-árok",
+    "start": [
+      47.6742901,
+      18.9711903
+    ],
+    "targetName": "Holdvilág-árok",
+    "target": [
+      47.6742901,
+      18.9711903
+    ],
+    "note": "Nagyon izgi, de technikásabb, csúszósabb/létrás részekkel. Két kicsivel inkább csak Döbivel.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/0/0e/Holdvilag-arok_-_panoramio.jpg",
+    "credit": "Wikimedia Commons",
+    "mapUrl": "https://maps.google.com/?q=47.6742901,18.9711903",
+    "startUrl": "https://maps.google.com/?q=47.6742901,18.9711903",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=Holdvil%C3%A1g-%C3%A1rok%20gyerekkel%20patak",
+    "gpxUrl": "gpx/holdvilag.gpx"
+  },
+  {
+    "id": "kiralyret",
+    "name": "Királyrét / Julianna-barlang / Szén-patak",
+    "area": "Szokolya",
+    "driveMin": 75,
+    "drive": "70–75 perc",
+    "distance": "mid",
+    "water": "közepes",
+    "dam": 5,
+    "toddler": "hordozó/kézenfogás",
+    "tag": "kisvasút",
+    "startName": "Julianna-barlang",
+    "start": [
+      47.9025031,
+      18.9825362
+    ],
+    "targetName": "Szén-patak környéke",
+    "target": [
+      47.9025031,
+      18.9825362
+    ],
+    "note": "A linkelt barlangos-patakos vibe. Kisvasúttal egész napos gyerekprogramnak nagyon erős.",
+    "image": "",
+    "credit": "",
+    "mapUrl": "https://maps.google.com/?q=47.9025031,18.9825362",
+    "startUrl": "https://maps.google.com/?q=47.9025031,18.9825362",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=Kir%C3%A1lyr%C3%A9t%20/%20Julianna-barlang%20/%20Sz%C3%A9n-patak%20gyerekkel%20patak",
+    "gpxUrl": "gpx/kiralyret.gpx"
+  },
+  {
+    "id": "apatkuti",
+    "name": "Apátkúti-völgy / Telgárthy-rét",
+    "area": "Visegrád",
+    "driveMin": 60,
+    "drive": "~60 perc",
+    "distance": "mid",
+    "water": "közepes",
+    "dam": 4,
+    "toddler": "hordozó/kézenfogás",
+    "tag": "piknik",
+    "startName": "Telgárthy-rét",
+    "start": [
+      47.7689812,
+      18.9824698
+    ],
+    "targetName": "Apátkúti-patak",
+    "target": [
+      47.7689812,
+      18.9824698
+    ],
+    "note": "Patak + rét + erdő, piknikezős családi nap. Röviden is működik, ha nem akartok nagy túrát.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Ap%C3%A1tk%C3%BAti-Bach%2C_Biegung%2C_2025_Visegr%C3%A1d.jpg/960px-Ap%C3%A1tk%C3%BAti-Bach%2C_Biegung%2C_2025_Visegr%C3%A1d.jpg",
+    "credit": "Wikimedia Commons",
+    "mapUrl": "https://maps.google.com/?q=47.7689812,18.9824698",
+    "startUrl": "https://maps.google.com/?q=47.7689812,18.9824698",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=Ap%C3%A1tk%C3%BAti-v%C3%B6lgy%20/%20Telg%C3%A1rthy-r%C3%A9t%20gyerekkel%20patak",
+    "gpxUrl": "gpx/apatkuti.gpx"
+  },
+  {
+    "id": "kismaros",
+    "name": "Kismaros / Morgó-patak",
+    "area": "Kismaros",
+    "driveMin": 80,
+    "drive": "70–85 perc",
+    "distance": "mid",
+    "water": "közepes",
+    "dam": 4,
+    "toddler": "helyfüggő",
+    "tag": "vonat",
+    "startName": "Kismaros vasútállomás",
+    "start": [
+      47.8277365,
+      19.0134108
+    ],
+    "targetName": "Morgó-patak Kismarosnál",
+    "target": [
+      47.8277365,
+      19.0134108
+    ],
+    "note": "Vonatos-kisvasutas-patakos program. Pontos partszakaszt érdemes előre kiválasztani.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Kismaros%2C_Morg%C3%B3-patak.jpg/960px-Kismaros%2C_Morg%C3%B3-patak.jpg",
+    "credit": "Wikimedia Commons",
+    "mapUrl": "https://maps.google.com/?q=47.8277365,19.0134108",
+    "startUrl": "https://maps.google.com/?q=47.8277365,19.0134108",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=Kismaros%20/%20Morg%C3%B3-patak%20gyerekkel%20patak",
+    "gpxUrl": "gpx/kismaros.gpx"
+  },
+  {
+    "id": "kemence",
+    "name": "Kemence / Csarna-patak / kisvasút",
+    "area": "Börzsöny",
+    "driveMin": 95,
+    "drive": "~95 perc",
+    "distance": "far",
+    "water": "közepes",
+    "dam": 5,
+    "toddler": "hordozó/kézenfogás",
+    "tag": "kisvasút",
+    "startName": "Kemence kisvasút",
+    "start": [
+      48.0158759,
+      18.8883868
+    ],
+    "targetName": "Csarna-patak / kisvasút",
+    "target": [
+      48.0158759,
+      18.8883868
+    ],
+    "note": "Egész napos Börzsöny-program: patak, kisvasút, erdei hangulat. Messzebb van, de nagyon gyerekbarát élmény.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Kemence%2C_kisvas%C3%BAt%2C_4.jpg/960px-Kemence%2C_kisvas%C3%BAt%2C_4.jpg",
+    "credit": "Wikimedia Commons",
+    "mapUrl": "https://maps.google.com/?q=48.0158759,18.8883868",
+    "startUrl": "https://maps.google.com/?q=48.0158759,18.8883868",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=Kemence%20/%20Csarna-patak%20/%20kisvas%C3%BAt%20gyerekkel%20patak",
+    "gpxUrl": "gpx/kemence.gpx"
+  },
+  {
+    "id": "tata",
+    "name": "Tata / Fényes tanösvény",
+    "area": "Tata",
+    "driveMin": 55,
+    "drive": "~55 perc",
+    "distance": "mid",
+    "water": "magas",
+    "dam": 2,
+    "toddler": "jó, de pallón figyelős",
+    "tag": "vizes tanösvény",
+    "startName": "Fényes tanösvény",
+    "start": [
+      47.6669022,
+      18.3068212
+    ],
+    "targetName": "Fényes tanösvény",
+    "target": [
+      47.6669022,
+      18.3068212
+    ],
+    "note": "Nem gátépítős patak, inkább biztos vizes-lápos-pallós tanösvény. Szárazságban is jobb esély vízélményre.",
+    "image": "",
+    "credit": "",
+    "mapUrl": "https://maps.google.com/?q=47.6669022,18.3068212",
+    "startUrl": "https://maps.google.com/?q=47.6669022,18.3068212",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=Tata%20/%20F%C3%A9nyes%20tan%C3%B6sv%C3%A9ny%20gyerekkel%20patak",
+    "gpxUrl": "gpx/tata.gpx"
+  },
+  {
+    "id": "cuha",
+    "name": "Cuha-völgy / Vinye",
+    "area": "Bakony",
+    "driveMin": 110,
+    "drive": "~1ó50p",
+    "distance": "far",
+    "water": "közepes",
+    "dam": 5,
+    "toddler": "hordozó/kézenfogás",
+    "tag": "egész nap",
+    "startName": "Vinye vasútállomás",
+    "start": [
+      47.3597696,
+      17.8198522
+    ],
+    "targetName": "Cuha-völgy",
+    "target": [
+      47.3597696,
+      17.8198522
+    ],
+    "note": "Patak, vasút, hidak, alagutak. Nagyon erős, de már nagy családi nap, nem spontán délután.",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Cuha-v%C3%B6lgy%2C_19.jpg/960px-Cuha-v%C3%B6lgy%2C_19.jpg",
+    "credit": "Wikimedia Commons",
+    "mapUrl": "https://maps.google.com/?q=47.3597696,17.8198522",
+    "startUrl": "https://maps.google.com/?q=47.3597696,17.8198522",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=Cuha-v%C3%B6lgy%20/%20Vinye%20gyerekkel%20patak",
+    "gpxUrl": "gpx/cuha.gpx"
+  },
+  {
+    "id": "gaja",
+    "name": "Gaja-szurdok",
+    "area": "Bodajk",
+    "driveMin": 75,
+    "drive": "70–80 perc",
+    "distance": "mid",
+    "water": "közepes",
+    "dam": 4,
+    "toddler": "hordozó/kézenfogás",
+    "tag": "szurdok",
+    "startName": "Gaja-szurdok",
+    "start": [
+      47.313765,
+      18.217227
+    ],
+    "targetName": "Gaja-patak / szurdok",
+    "target": [
+      47.313765,
+      18.217227
+    ],
+    "note": "Szép szurdokos-patakos hely hidakkal. Szárazságban víz mennyisége kérdéses.",
+    "image": "",
+    "credit": "",
+    "mapUrl": "https://maps.google.com/?q=47.313765,18.217227",
+    "startUrl": "https://maps.google.com/?q=47.313765,18.217227",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=Gaja-szurdok%20gyerekkel%20patak",
+    "gpxUrl": "gpx/gaja.gpx"
+  },
+  {
+    "id": "ram",
+    "name": "Rám-szakadék alsóbb részek / Dömös",
+    "area": "Dömös",
+    "driveMin": 55,
+    "drive": "~55 perc",
+    "distance": "mid",
+    "water": "közepes",
+    "dam": 3,
+    "toddler": "nem ideális",
+    "tag": "csak Döbivel",
+    "startName": "Dömös",
+    "start": [
+      47.7636231,
+      18.9110928
+    ],
+    "targetName": "Rám-szakadék",
+    "target": [
+      47.7387483,
+      18.8937988
+    ],
+    "note": "Klasszikus vizes-szurdokos hely, de a teljes Rám-szakadék két ilyen kicsivel még nem ideális. Csak óvatos, alsóbb részek.",
+    "image": "",
+    "credit": "",
+    "mapUrl": "https://maps.google.com/?q=47.7387483,18.8937988",
+    "startUrl": "https://maps.google.com/?q=47.7636231,18.9110928",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=R%C3%A1m-szakad%C3%A9k%20als%C3%B3bb%20r%C3%A9szek%20/%20D%C3%B6m%C3%B6s%20gyerekkel%20patak",
+    "gpxUrl": "gpx/ram.gpx"
+  },
+  {
+    "id": "remete",
+    "name": "Remete-szurdok",
+    "area": "Máriaremete",
+    "driveMin": 20,
+    "drive": "~20 perc",
+    "distance": "near",
+    "water": "alacsony",
+    "dam": 1,
+    "toddler": "kézenfogós/hordozó",
+    "tag": "közeli",
+    "startName": "Remete-szurdok",
+    "start": [
+      47.5593457,
+      18.9368346
+    ],
+    "targetName": "Remete-szurdok",
+    "target": [
+      47.5593457,
+      18.9368346
+    ],
+    "note": "Nem igazi patakos, de közeli árnyékos-köves mini-kaland. Jó mentőprogram, ha a víz nem feltétel.",
+    "image": "",
+    "credit": "",
+    "mapUrl": "https://maps.google.com/?q=47.5593457,18.9368346",
+    "startUrl": "https://maps.google.com/?q=47.5593457,18.9368346",
+    "imageSearchUrl": "https://www.google.com/search?tbm=isch&q=Remete-szurdok%20gyerekkel%20patak",
+    "gpxUrl": "gpx/remete.gpx"
+  }
+];
+window.APP_VERSION = "2026-08-02-3";
